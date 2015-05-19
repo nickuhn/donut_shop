a58cba9c95debf5a2172948b55b8c922958ed107
Nick Kuhn <nickuhn@gmail.comn>
2015-05-18 22:51:10 -0700

    Expanded daily method added paragraphs to html.
    Expanded the dailyDonuts method to include an open and close time to calculate the number of hours the store is open.
    Added some paragraphs with text for the index.html page.

--- tp_estimator.js
+++ tp_estimator.js
@@ -1,6 +1,7 @@
 (function(){
+  //declare all the variables and the column headers.
   var downtown, capitolHill, southLakeUnion, wedgewood, ballard;
-  var tableValues = [' ', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', 'Total']
+  var tableValues = [' ', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', 'Total']
 
   //Shop object constructor with prototype methods for determining a random
   //customer per hour count, and based on that the donuts needed per hour and
@@ -20,16 +21,24 @@
   Shop.prototype.hourlyDonuts = function() {
     return parseInt((this.avgDonutPerCust * this.custPerHour(this.minCustPerHour, this.maxCustPerHour)));
   }
-  Shop.prototype.dailyDonuts = function() {
-    var total  = 0;
-    var hourly = 0;
-    for (var i = 12; i > 0; i--) {
+  //Calculates the daily donuts needed by calling the hourly method for each hour
+  //the store is open. It stores each of the hourly calls in an array for reference
+  //in the render method. And then stores the total to the object.
+  //Open and close times need to be in military time rounded to nearest hour.
+  Shop.prototype.dailyDonuts = function(open, close) {
+    var total     = 0;
+    var hourly    = 0;
+    var hoursOpen = close - open;
+    for (var i = hoursOpen; i > 0; i--) {
       hourly          = this.hourlyDonuts();
       this.byHour[i]  = hourly;
       total          += hourly;
     }
     this.byHour[0] = total;
   }
+  //Creates a row on the table for each object call. The first element is the
+  //location name in a <th> element tag. The rest are iterated over and stored
+  //in <td> elements. The row is then appended to the table.
   Shop.prototype.render = function(name) {
     var table    = document.getElementById('table-body');
     var row      = document.createElement('tr');
@@ -46,6 +55,8 @@
     table.appendChild(row);
   }
 
+  //Initializes the table. An array of column headers is passed in and then
+  //iterated over to build a row of <th> elements
   tableInit = function() {
     var tableHead  = document.getElementById('table-head');
     var row        = document.createElement('tr');
@@ -58,31 +69,33 @@
     tableHead.appendChild(row);
   }
 
-  //Initialize the table and instantiates 5 shop locations.
+  //Initialize the table and instantiates 5 shop locations. The daily method is
+  //called to create the estimates and then the renders is called to print them
+  //to the table on the index.html page.
   tableInit();
 
   downtown       = new Shop(8, 43, 4.50);
   downtown.custPerHour(downtown.minCustPerHour, downtown.maxCustPerHour);
-  downtown.dailyDonuts();
+  downtown.dailyDonuts(7, 18);
   downtown.render("Downtown");
 
   capitolHill    = new Shop(4, 37, 2.00);
   capitolHill.custPerHour(capitolHill.minCustPerHour, capitolHill.maxCustPerHour);
-  capitolHill.dailyDonuts();
+  capitolHill.dailyDonuts(7, 18);
   capitolHill.render("Capitol Hill");
 
   southLakeUnion = new Shop(9, 23, 6.33);
   southLakeUnion.custPerHour(southLakeUnion.minCustPerHour, southLakeUnion.maxCustPerHour);
-  southLakeUnion.dailyDonuts();
+  southLakeUnion.dailyDonuts(7, 18);
   southLakeUnion.render("South Lake Union");
 
   wedgewood      = new Shop(2, 28, 1.25);
   wedgewood.custPerHour(wedgewood.minCustPerHour, wedgewood.maxCustPerHour);
-  wedgewood.dailyDonuts();
+  wedgewood.dailyDonuts(7, 18);
   wedgewood.render("Wedgewood");
 
   ballard        = new Shop(8, 58, 3.75);
   ballard.custPerHour(ballard.minCustPerHour, ballard.maxCustPerHour);
-  ballard.dailyDonuts();
+  ballard.dailyDonuts(7, 18);
   ballard.render("Ballard");
 })();
