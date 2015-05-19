9fc75b33e048ae86049e24f3121837c52a5551e9
Nick Kuhn <nickuhn@gmail.comn>
2015-05-18 22:36:27 -0700

    Refactored Render method, added table head and body.
    Refactored the render method to use createElement and createTextNode in order to specify the headings as <th> elements instead of <td>.
    Added the table head and table body tags to the table element in my html for future styling needs and to better match conventions.

--- tp_estimator.js
+++ tp_estimator.js
@@ -2,58 +2,71 @@
   var downtown, capitolHill, southLakeUnion, wedgewood, ballard;
   var tableValues = [' ', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', 'Total']
 
+  //Shop object constructor with prototype methods for determining a random
+  //customer per hour count, and based on that the donuts needed per hour and
+  //per day. The results of these methods are then used in the render and
+  //tableInit function to write a table to the index.html page dynamically.
   function Shop(minCustHour, maxCustHour, donutPerCust){
-    this.minCustPerHour = minCustHour;
-    this.maxCustPerHour = maxCustHour;
+    this.minCustPerHour  = minCustHour;
+    this.maxCustPerHour  = maxCustHour;
     this.avgDonutPerCust = donutPerCust;
-    this.byHour = [];
+    this.byHour          = [];
   }
+
   Shop.prototype.custPerHour = function(min,max){
-    var total = 0
-    total = Math.floor(Math.random() * (max - min + 1)) + min;
-    return total;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
   Shop.prototype.hourlyDonuts = function() {
-    var donutsNeeded = 0;
-    donutsNeeded = parseInt((this.avgDonutPerCust * this.custPerHour(this.minCustPerHour, this.maxCustPerHour)));
-    return donutsNeeded;
+    return parseInt((this.avgDonutPerCust * this.custPerHour(this.minCustPerHour, this.maxCustPerHour)));
   }
   Shop.prototype.dailyDonuts = function() {
-    var total = 0;
+    var total  = 0;
     var hourly = 0;
     for (var i = 12; i > 0; i--) {
-      hourly = this.hourlyDonuts();
-      this.byHour[i] = hourly;
-      total += hourly;
+      hourly          = this.hourlyDonuts();
+      this.byHour[i]  = hourly;
+      total          += hourly;
     }
     this.byHour[0] = total;
   }
   Shop.prototype.render = function(name) {
-    var table = document.getElementById('main-table');
-    var row = table.insertRow(1);
-    for (i = 0; i < this.byHour.length; i++) {
-      row.insertCell(0).textContent = this.byHour[i];
+    var table    = document.getElementById('table-body');
+    var row      = document.createElement('tr');
+    var cell     = document.createElement('th');
+    var cellText = document.createTextNode(name);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+    for (var i = this.byHour.length - 1; i >= 0; i--) {
+      cell       = document.createElement('td');
+      cellText   = document.createTextNode(this.byHour[i]);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
     }
-    row.insertCell(0).textContent = name;
+    table.appendChild(row);
   }
 
   tableInit = function() {
-    var table = document.getElementById('main-table');
-    var row = table.insertRow(0);
-    for (i = tableValues.length; i >= 0; i--) {
-      row.insertCell(0).textContent = tableValues[i];
+    var tableHead  = document.getElementById('table-head');
+    var row        = document.createElement('tr');
+    for (var i = 0; i < tableValues.length; i++) {
+      var cell     = document.createElement('th');
+      var cellText = document.createTextNode(tableValues[i]);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
     }
+    tableHead.appendChild(row);
   }
 
-
+  //Initialize the table and instantiates 5 shop locations.
   tableInit();
 
-  downtown = new Shop(8, 43, 4.50);
+  downtown       = new Shop(8, 43, 4.50);
   downtown.custPerHour(downtown.minCustPerHour, downtown.maxCustPerHour);
   downtown.dailyDonuts();
   downtown.render("Downtown");
 
-  capitolHill = new Shop(4, 37, 2.00);
+  capitolHill    = new Shop(4, 37, 2.00);
   capitolHill.custPerHour(capitolHill.minCustPerHour, capitolHill.maxCustPerHour);
   capitolHill.dailyDonuts();
   capitolHill.render("Capitol Hill");
@@ -63,12 +76,12 @@
   southLakeUnion.dailyDonuts();
   southLakeUnion.render("South Lake Union");
 
-  wedgewood = new Shop(2, 28, 1.25);
+  wedgewood      = new Shop(2, 28, 1.25);
   wedgewood.custPerHour(wedgewood.minCustPerHour, wedgewood.maxCustPerHour);
   wedgewood.dailyDonuts();
   wedgewood.render("Wedgewood");
 
-  ballard = new Shop(8, 58, 3.75);
+  ballard        = new Shop(8, 58, 3.75);
   ballard.custPerHour(ballard.minCustPerHour, ballard.maxCustPerHour);
   ballard.dailyDonuts();
   ballard.render("Ballard");
